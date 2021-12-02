588276d8fd7a4e93a2153d9ace94835e4d3ff379
AjChaudhary000 <arjun910661@gmail.com>
2021-12-03 04:42:22 +0530

    all update save

--- Assignment_practical_5/Modal/SellerModal.js
+++ Assignment_practical_5/Modal/SellerModal.js
@@ -1,8 +1,8 @@
 const mongose = require('mongoose');
 const SellerSchema = mongose.Schema({
-    CompanyId:String,
+    SellerId:String,
     Name:String,
     ProductId:[{type:String}],
 });
-const SellerModal = mongose.model("Product",SellerSchema,"Seller")
+const SellerModal = mongose.model("assignmnet5",SellerSchema,"Seller")
 module.exports = SellerModal;
--- Assignment_practical_5/Router/SellerRouter.js
+++ Assignment_practical_5/Router/SellerRouter.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-const ProductModal = require('../Modal/ProductModal')
+const SellerModal = require('../Modal/SellerModal')
 mongoose.connect(process.env.MONGOOES_URL).then(()=>console.log("connected "));
 router.use(express.json());
 router.post('/',(req,res)=>{
-    return res.json({data:'Product Page ...'})
+    return res.json({data:'seller Page ...'})
 });
-router.post('/AddProducts',(req,res)=>{
+router.post('/AddSeller',(req,res)=>{
     const record = req.body;
-    ProductModal.create(record)
-    return res.json({data:'Product Data Add Sucessfully .....'})
+    SellerModal.create(record)
+    return res.json({data:'Seller Data Add Sucessfully .....'})
 });
 module.exports = router;
