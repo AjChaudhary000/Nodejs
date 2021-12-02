cff62766f821c81398f1fd44eeea9f9bdbed684f
arjun <arjun910661@gmail.com>
2021-12-02 09:08:14 +0530

    ProductModalChange

--- Assignment_practical_5/Modal/ProductModal.js
+++ Assignment_practical_5/Modal/ProductModal.js
@@ -3,9 +3,9 @@ const ProductShema = mongose.Schema({
     ProductId:String,
     Title:String,
     Price:String,
-    Category:[{type:String}],
+    Category:[String],
     CompanyId:String,
-    SellerId:[{type:String}],
+    SellerId:[String],
 });
-mongose.model("Products",ProductShema,"Product")
+const ProductModal = mongose.model("Products",ProductShema,"Product")
 module.exports = ProductModal;
