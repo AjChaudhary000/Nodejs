c231e01e1e7ac8408327350bd5cbb5b2cb0a8dd4
arjun chaudhary <81225289+AjChaudhary000@users.noreply.github.com>
2021-12-03 11:03:01 +0000

    all change save

--- Assignment_practical_5/Router/ProductRouter.js
+++ Assignment_practical_5/Router/ProductRouter.js
@@ -5,11 +5,27 @@ const ProductModal = require('../Modal/ProductModal')
 // mongoose.connect(process.env.MONGOOES_URL).then(()=>console.log("connected "));
 router.use(express.json());
 router.post('/',(req,res)=>{
-    return res.json({data:'Product Page ...'})
+    return res.json({data:'Product  ...'})
 });
 router.post('/AddProducts',(req,res)=>{
     const record = req.body;
     ProductModal.create(record)
     return res.json({data:'Product Data Add Sucessfully .....'})
+});
+router.post('/Display',async(req,res)=>{
+    const dt = await ProductModal.find();
+    return res.json({data:dt})
+});
+router.put('/UptProducts/:ProductId',async(req,res)=>{
+  const id = req.params.ProductId;
+    const Title = req.body.Title;
+    const Price = req.body.Price;
+    const Category = req.body.Category;
+    const CompanyId = req.body.CompanyId;
+    const SellerId = req.body.SellerId;
+    const data = {Title:Title,Price:Price,Category:Category,CompanyId:CompanyId,SellerId:SellerId}
+    console.log(data)
+   const update =await ProductModal.findOneAndUpdate({ProductId:id},data,{new:true})
+    return res.json({data:'Product Data Update Sucessfully .....'})
 });
 module.exports = router;
