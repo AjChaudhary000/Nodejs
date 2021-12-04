daa7ed58ad72e19569728016b6844453da082765
AjChaudhary000 <arjun910661@gmail.com>
2021-12-05 00:21:43 +0530

    db set

--- Assignment_6/index.js
+++ Assignment_6/index.js
@@ -3,8 +3,11 @@ const app = express()
 const port = 3000
 const CarCate = require('./Router/CarCateRouter'); 
 const CarDetail = require('./Router/CarDetailsRouter');
+
 const  mongose = require('mongoose');
 require('dotenv').config();
+mongose.connect(process.env.MongoseUrl).then((req,res)=>console.log("connect"));
+
 app.use("/CarCate",CarCate);
 app.use("/CarDetail",CarDetail);
 app.get('/', (req, res) => res.send('Hello World!'))
