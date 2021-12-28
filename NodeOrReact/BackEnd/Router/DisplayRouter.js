35d9254e94793b650b92b5ffe6e792413b3cbf77
arjun chaudhary <81225289+AjChaudhary000@users.noreply.github.com>
2021-12-28 08:17:51 +0530

    del3

--- NodeOrReact/BackEnd/Router/DisplayRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router();
-router.use(express.json());
-const UserModal = require("../Modal/UsersModal");
-router.post("/StudentDetails",async(req,res)=>{
-    const list  = await UserModal.find();
-   return  res.json({data:list}) 
-})
-module.exports = router;
